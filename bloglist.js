839c3f21f740520c1f05172065f2c0f2d5e39173
yetaai <dongjin.du@gmail.com>
2015-04-14 17:05:15 -0500

    brandNew

---
+++ bloglist.js
@@ -0,0 +1,14 @@
+<div class="eggTray">
+<script src="http://helplogger.googlecode.com/svn/trunk/listbadge.js">{"pipe_id":"1a6640e2a78b2c6e736f2220529daae5","_btype":"list",
+"pipe_params":{"URL":"http://yetaai.blogspot.com/feeds/posts/default"},
+"hideHeader":"false","height":"500","count": 16 }</script>
+<div style="font-family: arial, sans-serif; font-size: 9px;" class="ycdr"><a href="http://helplogger.blogspot.com/2012/05/recent-posts-widget-with-thumbnails-for.html" target="_blank" title="Grab this widget">Recent Posts Thumbnails</a> <a href="http://helplogger.blogspot.com/" target="_blank">Blogger Widget</a></div><noscript>Your browser does not support JavaScript!</noscript></div>
+<style type=text/css>
+.eggTray {margin:10px 0px;padding:0px;}
+.ybr li  {border-bottom:0px #cccccc dotted; padding:0px 0px 10px 0px!important;}
+.pipesTitle {padding-top:10px;}
+.pipesDescription {display:none;}
+.ycdr {background:transparent url(http://3.bp.blogspot.com/-oxTuqVj1ziA/T6P6wtxqsgI/AAAAAAAACBE/wWR0bb2gfuE/s1600/logo.png) 0px 0px no-repeat; padding: 1px 0px 0px 19px; height:14px; margin: 4px 0px 0px 0px;line-height:14px;}
+.ycdr, .ycdr a {color:#999999;}
+.widget .popular-posts ul {padding-left:0;}
+</style>
